1add884e0dd9d5d0bb82715d6c88aed55500139a
Atila <53686150+atila-tokyo@users.noreply.github.com>
2021-10-08 17:32:01 +0900

    Update footer.php

--- src/Views/footer.php
+++ src/Views/footer.php
@@ -1,4 +1,4 @@
  <div class="text-center p-3" style="background-color: black">
-      © 2021 Copyright:
+      &copy; 2021 Copyright:
       <a class="text-white" href="https://github.com/atila-tokyo/">Search CEP</a>
  </div>
